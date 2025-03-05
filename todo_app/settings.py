fc33a08337b820661f7613aad8f9feeed8d08fd0
SHAMMAS <muhdshammas612@gmail.com>
2025-03-05 22:34:39 +0530

    requirements added

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ob#*2)^or5$p8-#7_^+5$$g^4o@_(z*pftqhyb&5gk&eo*#wfm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
